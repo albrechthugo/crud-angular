cbc2887e24a2f90926c33a7b18b4804bfe63df4c
Hugo <albrechthugo4@gmail.com>
2020-11-15 21:13:11 -0300

    adding the first routes of the application

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+ 
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
