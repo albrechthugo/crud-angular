abd8400216638a4cbd452d8cfe3cbd5400e0599c
Hugo <albrechthugo4@gmail.com>
2020-11-30 22:07:06 -0300

    add skeleton loading, delete and edit product

--- frontend/src/app/components/product/list-products/list-products.component.ts
+++ frontend/src/app/components/product/list-products/list-products.component.ts
@@ -9,14 +9,40 @@ import { Product } from "../product.model";
 })
 export class ListProductsComponent implements OnInit {
 
+  loadingProductList: boolean = false;
+
   products: Product[];
+
+  product: Product = {
+    name: '',
+    price: null,
+    description: '',
+    specifications: ''
+  }
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProductList().subscribe(products => {
       this.products = products;
-      console.log(this.products);
+    })
+
+    this.productService.getProductList().subscribe(products => {
+      if(products != null) {
+        this.loadingProductList = true;
+      }
+    })
+  }
+
+  handleEditClick(product: Product): void {
+    this.productService.editProduct(product).subscribe(() => {
+      this.productService.showConfirmationPopUp('Produto EDITADO com sucesso!');
+    })
+  }
+
+  handleDeleteClick(id: number): void {
+    this.productService.deleteProduct(id).subscribe(() => {
+      this.productService.showConfirmationPopUp('Produto DELETADO com sucesso!');
     })
   }
 
