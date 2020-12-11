4050a3128b15ed993e2d120fb0433a9549e4cfbc
Hugo <albrechthugo4@gmail.com>
2020-12-11 10:32:41 -0300

    adding new page in application

---
+++ frontend/src/app/pages/show-product/show-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-product',
+  templateUrl: './show-product.component.html',
+  styleUrls: ['./show-product.component.css']
+})
+export class ShowProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
