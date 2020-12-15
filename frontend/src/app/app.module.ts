bd83d01a024bb422999fe5402484fc86c1fce6d6
albrechthugo <albrechthugo4@gmail.com>
2020-12-15 17:40:24 -0300

    cleaning principal module and importing product module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
-
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { ProductModule } from './modules/product/product.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
-import { CreateProductComponent } from './components/product/create-product/create-product.component';
 
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { ListProductsComponent } from './components/product/list-products/list-products.component';
-import { ProductLoadingComponent } from './components/product/product-loading/product-loading.component';
-import { ActionButtonComponent } from './components/buttons/action-button/action-button.component';
-import { EditProductComponent } from './components/product/edit-product/edit-product.component';
 import { ShowProductComponent } from './pages/show-product/show-product.component';
-import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
-import { EmptyCartComponent } from './components/utils/empty-cart/empty-cart.component';
+
 
 @NgModule({
   declarations: [
@@ -33,26 +22,14 @@ import { EmptyCartComponent } from './components/utils/empty-cart/empty-cart.com
     FooterComponent,
     HomeComponent,
     ProductsComponent,
-    CreateProductComponent,
-    ListProductsComponent,
-    ProductLoadingComponent,
-    ActionButtonComponent,
-    EditProductComponent,
     ShowProductComponent,
-    ProductDetailsComponent,
-    EmptyCartComponent,
-    ActionButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
+    ProductModule,
     MatCardModule,
-    MatFormFieldModule,
-    MatSnackBarModule,
-    HttpClientModule,
-    FormsModule
+    MatToolbarModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
