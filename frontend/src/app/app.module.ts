4dabdd352729be9639bf3514c4579376fb1d1420
Hugo <hugo.andreas@totvs.com.br>
2020-12-16 14:27:03 -0200

    importing pages on product module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,20 +9,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
-
-import { HomeComponent } from './pages/home/home.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ShowProductComponent } from './pages/show-product/show-product.component';
-
+import { ActionButtonComponent } from './components/buttons/action-button/action-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    ProductsComponent,
-    ShowProductComponent,
+    ActionButtonComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/modules/product/product.module.ts
+++ frontend/src/app/modules/product/product.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
-import { ActionButtonComponent } from 'src/app/components/buttons/action-button/action-button.component';
 import { EmptyCartComponent } from 'src/app/components/utils/empty-cart/empty-cart.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { ListProductsComponent } from './list-products/list-products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductLoadingComponent } from './product-loading/product-loading.component';
+
+import { HomeComponent } from 'src/app/pages/home/home.component';
+import { ProductsComponent } from 'src/app/pages/products/products.component';
+import { ShowProductComponent } from 'src/app/pages/show-product/show-product.component';
+import { NotFoundComponent } from 'src/app/pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -26,10 +30,13 @@ import { ProductLoadingComponent } from './product-loading/product-loading.compo
     ProductDetailsComponent,
     ProductLoadingComponent,
     EmptyCartComponent,
-    ActionButtonComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShowProductComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule,
+    CommonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
@@ -46,7 +53,10 @@ import { ProductLoadingComponent } from './product-loading/product-loading.compo
     ProductDetailsComponent,
     ProductLoadingComponent,
     EmptyCartComponent,
-    ActionButtonComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShowProductComponent,
+    NotFoundComponent,
   ],
   providers: []
 })
