98b9fc79ebe9b9cf270b2b2d9e62cb080494133c
Hugo <hugo.andreas@totvs.com.br>
2020-12-16 18:22:48 -0200

    exports global components

---
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+import { ActionButtonComponent } from 'src/app/components/buttons/action-button/action-button.component';
+import { FooterComponent } from 'src/app/components/template/footer/footer.component';
+import { HeaderComponent } from 'src/app/components/template/header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ActionButtonComponent,
+    FooterComponent
+  ],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    ActionButtonComponent,
+    FooterComponent
+  ],
+  providers: []
+})
+
+export class SharedModule {}
